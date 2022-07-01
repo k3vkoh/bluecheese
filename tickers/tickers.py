0a392930845bf699130d6101a36368357b113caf
k3vkoh <kev33koh@gmail.com>
2022-06-30 17:22:15 -0700

    tickers updated

--- tickers/tickers.py
+++ tickers/tickers.py
@@ -1,14 +1,17 @@
 def get_tickers():
-	f = open('ticker.csv', 'r')
-	t_list = open('tickers.txt', 'w')
 
-	for line in f:
-		temp = line.split(',')
-		if temp[0] != 'Symbol':
-			t_list.write('{}\n'.format(temp[0]))			
+	with open('t1.csv', 'r') as t1, open('t2.csv', 'r') as t2, open('tickers.txt', 'w') as t:
 
-	f.close()
-	t_list.close()
+		for line in t1:
+			temp = line.split(',')
+			if temp[0] != 'Symbol':
+				t.write('{}\n'.format(temp[0]))	
+
+		for line in t2:
+			temp = line.split(',')
+			if temp[0] != 'Symbol':
+				t.write('{}\n'.format(temp[0]))			
+
 
 
 def main():
