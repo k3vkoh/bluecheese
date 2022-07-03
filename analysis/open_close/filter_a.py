4f7ec0bb47d64b7d42cbd2b58b0119d0d8765cc7
k3vkoh <kev33koh@gmail.com>
2022-07-03 01:09:26 -0700

    testing consecutive days

--- analysis/open_close/filter_a.py
+++ analysis/open_close/filter_a.py
@@ -1,8 +1,6 @@
 # filter a:
 # compiles a list of tickers that meet the following criteria:
-# redo with 10 days and then 5 days then run the simulation
  
-
 import pandas as pd 
 from sqlalchemy.types import Text
 from sqlalchemy import create_engine
@@ -31,25 +29,27 @@ tentative = os.path.join(cwd, 'tickers/tentative', '{}.txt'.format(today_string)
 ticker_list = os.path.join(cwd, 'tickers', 'tickers.txt')
 bargraph = os.path.join(cwd, 'analysis/open_close/bargraph')
 
-price_limit = 50
+sql = """
+		SELECT * FROM daily 
+		WHERE Ticker = 'AAPL'
+	"""
 
-limit = 5
+totaldf = pd.read_sql(sql, engine)
 
-tentative_list = []
+rowcount = totaldf.shape[0]
 
 def get_data(ticker):
 	sql = """
 			SELECT * FROM daily 
 			WHERE Ticker = "{}"
 			ORDER BY time DESC
-			LIMIT {}
-		""".format(ticker, limit)
+		""".format(ticker)
 
 	df = pd.read_sql(sql, engine)
 
 	return df
 
-def price_filter():
+def consecutive():
 
 	files = glob.glob(os.path.join(bargraph, '*'))
 	for f in files:
@@ -57,58 +57,186 @@ def price_filter():
 
 	with open(tentative, 'w') as f, open(ticker_list, 'r') as t:
 
-		f.write('Filter: Price Filter\n')
-
 		# for temp in ['AAPL']:
 		for temp in t:
+
+			tentative_list = [] 
+
 			ticker = temp.strip()
 
 			df = get_data(ticker)
 
-			if len(df['open']):
-				avg_price = statistics.mean(df['open'])
-				if avg_price >= price_limit:
-					f.write('{}, avg price: {}\n'.format(ticker, avg_price))
-					f.flush()
-					calculate(ticker, df)
+			if df.shape[0] != rowcount:
+				continue
 
-def calculate(ticker, df):
+			open_close = df['close'] - df['open']
 
-	# y is equal to open_close values
-	y = ((df['close'] - df['open'])/df['open']) * 100
-	x = range(0, len(y))
+			pd.set_option('display.max_rows', None)
 
-	avg = statistics.mean(y)
+			i = rowcount - 1
+			while i > 0:
+				consecutive_neg = 0
+				consecutive_pos = 0
+				sign_change = False
+				while not sign_change and i > 0:
+					if open_close[i] > 0:
+						consecutive_pos += 1
+					else:
+						consecutive_neg += 1
+					today = open_close[i] > 0
+					tomorrow = open_close[i-1] > 0
+					if today != tomorrow:
+						sign_change = True
+						if consecutive_pos:
+							tentative_list.append([ticker, '+', consecutive_pos])
+						else:
+							tentative_list.append([ticker, '-', consecutive_neg])
+					else:
+						i -= 1
+						if i == 0:
+							if consecutive_pos:
+								tentative_list.append([ticker, '+', consecutive_pos])
+							else:
+								tentative_list.append([ticker, '-', consecutive_neg])
+				i -= 1
 
-	# consecutive = [value < 0 for value in y]
-	# if False not in consecutive:
-	# 	tentative_list.append(ticker)
+			f.write("{}\n".format(ticker))
+			for x in tentative_list:
+				f.write('{} {} consecutive days\n'.format(x[2], x[1]))
 
-	if avg >= 0.5:
-		tentative_list.append([ticker, avg])
-	
-	plt.plot(x, y)
-	# plt.bar(positive['x'], positive['y'], color = 'Green')
-	# plt.bar(negative['x'], negative['y'], color = 'Red')
-	# plt.plot([0, len(x)-1], [high_avg, high_avg], 'g+')
-	plt.plot([0, len(x)-1], [avg, avg], 'k--')
-	# plt.plot([0, len(x)-1], [low_avg, low_avg], 'r-')
+			neg = list(filter(lambda x: x[1] == '-', tentative_list))
+			pos = list(filter(lambda x: x[1] == '+', tentative_list))
 
-	plt.savefig(os.path.join(bargraph, '{}.png'.format(ticker)))
+			neg_num_list = [x[2] for x in neg]
+			pos_num_list = [x[2] for x in pos]
+			total = [x[2] for x in tentative_list]
 
-	plt.close()
+			neg_avg = statistics.mean(neg_num_list)
+			pos_avg = statistics.mean(pos_num_list)
 
-def rank_tentative():
-	tentative_list.sort(key = lambda x: x[1], reverse = True)
+			f.write('avg consecutive - days: {}\n'.format(neg_avg))
+			f.write('avg consecutive + days: {}\n'.format(pos_avg))
+			f.write('total days: {}\n'.format(sum(total)))
 
-	with open(tentative, 'a') as f:
-
-		f.write('\nFilter: Avg\n')
-		for item in tentative_list:
-			f.write('{}, Avg: {}\n'.format(item[0], item[1]))
 
 
 def run():
-	price_filter()
-	rank_tentative()
+	consecutive()
+
+
+
+# import pandas as pd 
+# from sqlalchemy.types import Text
+# from sqlalchemy import create_engine
+
+# import matplotlib
+# import matplotlib.pyplot as plt
+
+# import numpy as np
+# from scipy.stats import norm
+# import statistics
+
+# from datetime import datetime
+
+# import os
+# import requests
+# import glob
+
+# engine = create_engine('sqlite:////Users/kevinkoh/Desktop/bluecheese/bluecheese.db')
+
+# today = datetime.now()
+# today_string = today.strftime('%Y-%m-%d')
+
+# cwd = os.getcwd()
+
+# tentative = os.path.join(cwd, 'tickers/tentative', '{}.txt'.format(today_string))
+# ticker_list = os.path.join(cwd, 'tickers', 'tickers.txt')
+# bargraph = os.path.join(cwd, 'analysis/open_close/bargraph')
+
+# price_limit = 50
+
+# limit = 30
+
+# tentative_list = []
+
+# def get_data(ticker):
+# 	sql = """
+# 			SELECT * FROM daily 
+# 			WHERE Ticker = "{}"
+# 			ORDER BY time DESC
+# 			LIMIT {}
+# 		""".format(ticker, limit)
+
+# 	df = pd.read_sql(sql, engine)
+
+# 	return df
+
+# def price_filter():
+
+# 	files = glob.glob(os.path.join(bargraph, '*'))
+# 	for f in files:
+# 		os.remove(f)
+
+# 	with open(tentative, 'w') as f, open(ticker_list, 'r') as t:
+
+# 		f.write('Filter: Price Filter\n')
+
+# 		# for temp in ['AAPL']:
+# 		for temp in t:
+# 			ticker = temp.strip()
+
+# 			df = get_data(ticker)
+
+# 			if len(df['open']) >= (limit/2):
+# 				avg_price = statistics.mean(df['open'])
+# 				if avg_price >= price_limit:
+# 					f.write('{}, avg price: {}\n'.format(ticker, avg_price))
+# 					f.flush()
+# 					calculate(ticker, df)
+
+# def calculate(ticker, df):
+
+# 	# y is equal to open_close values
+# 	y1 = ((df['close'] - df['open'])/df['open']) * 100
+# 	y2 = df['open']
+# 	# x = [ x.split()[0] for x in df['date']]
+# 	x = range(0, len(y1))
+
+# 	# avg = statistics.mean(y)
+
+# 	# consecutive = [value < 0 for value in y]
+# 	# if False not in consecutive:
+# 	# 	tentative_list.append(ticker)
+
+# 	# if avg >= 0.5:
+# 	# 	tentative_list.append([ticker, avg])
+
+# 	fig, ax1 = plt.subplots()
+
+# 	ax2 = ax1.twinx()
+
+# 	ax1.bar(x, y1)
+# 	ax2.plot(x, y2, 'b-')
+
+# 	ax1.set_xlabel('X data')
+# 	ax1.set_ylabel('close open %', color='g')
+# 	ax2.set_ylabel('open price', color='b')
+
+# 	plt.savefig(os.path.join(bargraph, '{}.png'.format(ticker)))
+
+# 	plt.close()
+
+# def rank_tentative():
+# 	tentative_list.sort(key = lambda x: x[1], reverse = True)
+
+# 	with open(tentative, 'a') as f:
+
+# 		f.write('\nFilter: Avg\n')
+# 		for item in tentative_list:
+# 			f.write('{}, Avg: {}\n'.format(item[0], item[1]))
+
+
+# def run():
+# 	price_filter()
+# 	rank_tentative()
 
