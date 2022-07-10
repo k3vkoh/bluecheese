45c04e3ae67e9bda8687893afe49c7f02587ef69
k3vkoh <kev33koh@gmail.com>
2022-07-10 01:12:34 -0700

    new tickers

--- tickers/tickers.py
+++ tickers/tickers.py
@@ -1,16 +1,17 @@
 def get_tickers():
 
-	with open('nasdaq.csv', 'r') as nd, open('nyse.csv', 'r') as ny, open('tickers.txt', 'w') as t:
+	# with open('nasdaq.csv', 'r') as nd, open('nyse.csv', 'r') as ny, open('tickers.txt', 'w') as t:
+	with open('nasdaq.csv', 'r') as nd, open('tickers.txt', 'w') as t:
 
 		for line in nd:
 			temp = line.split(',')
 			if temp[0] != 'Symbol':
 				t.write('{}\n'.format(temp[0]))	
 
-		for line in ny:
-			temp = line.split(',')
-			if temp[0] != 'Symbol':
-				t.write('{}\n'.format(temp[0]))	
+		# for line in ny:
+		# 	temp = line.split(',')
+		# 	if temp[0] != 'Symbol':
+		# 		t.write('{}\n'.format(temp[0]))	
 
 def main():
 	get_tickers()
